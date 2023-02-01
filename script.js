5736689ef75c9403a3341ba7dad183ea1ed4049d
Charnetta Smith <110750951+acktivegame@users.noreply.github.com>
2023-02-01 14:21:08 -0600

    updated character

---
+++ script.js
@@ -0,0 +1,62 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 4;
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+];
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let position =
+    Math.floor(gameFrame / staggerFrames) % spriteAnimations["idle"].loc.length;
+  frameX = spriteWidth * position;
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+  if (gameFrame % staggerFrames == 0) {
+    if (frameX < 4) frameX++;
+    else frameX = 0;
+  }
+
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+animate();
